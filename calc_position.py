00b8f2d6379a222a8f26a6e95bbb28739956839c
jrandall24 <jrandall27@gcds.net>
2026-02-19 13:19:51 -0500

    updation

--- calc_position.py
+++ calc_position.py
@@ -3,23 +3,22 @@
 G = 6.67*(10**-11)
 
 class planet:
-    def __init__(self):
-        self.T = 10
-        self.M_host = 1.989e30
-        self.X = 150000000
+    def __init__(self, M, X, Vy):
+        self.T = .5
+        self.M = M
+        self.X = X
         self.Y = 0
-        self.ax = -1*G*self.M_host*self.X/((self.X**2+self.Y**2)**1.5)
-        self.ay = -1*G*self.M_host*self.Y/((self.X**2+self.Y**2)**1.5)
+        self.ax = -1*G*self.M*self.X/((self.X**2+self.Y**2)**1.5)
+        self.ay = -1*G*self.M*self.Y/((self.X**2+self.Y**2)**1.5)
         self.Vx = 0
-        self.Vy = 581000
+        self.Vy = Vy
 
     def calc_pos(self):
-        p = self
         global G
-        p.X = p.X+(p.Vx*p.T)
-        p.Y = p.Y+(p.Vy*p.T)
-        p.ax = -1*G*p.M_host*p.X/((p.X**2+p.Y**2)**1.5)
-        p.ay = -1*G*p.M_host*p.Y/((p.X**2+p.Y**2)**1.5)
-        p.Vx = p.Vx+p.ax*p.T
-        p.Vy = p.Vy+p.ay*p.T
-        return (p.X, p.Y)
+        self.X = self.X+(self.Vx*self.T)
+        self.Y = self.Y+(self.Vy*self.T)
+        self.ax = -1*G*self.M*self.X/((self.X**2+self.Y**2)**1.5)
+        self.ay = -1*G*self.M*self.Y/((self.X**2+self.Y**2)**1.5)
+        self.Vx = self.Vx+self.ax*self.T
+        self.Vy = self.Vy+self.ay*self.T
+        return (self.X, self.Y)
