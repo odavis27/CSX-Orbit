b7788314fe7923ca5fac6497b8216180c07499b3
jrandall24 <jrandall27@gcds.net>
2026-02-19 18:14:29 -0500

    updation

--- calc_position.py
+++ calc_position.py
@@ -1,24 +1,36 @@
-#from simulation_class import *
-
-G = 6.67*(10**-11)
+G = 6.67e-11
 
 class planet:
-    def __init__(self, M, X, Vy):
+    def __init__(self, MH, XP, VyP, MP, XH, VyH):
         self.T = .5
-        self.M = M
-        self.X = X
-        self.Y = 0
-        self.ax = -1*G*self.M*self.X/((self.X**2+self.Y**2)**1.5)
-        self.ay = -1*G*self.M*self.Y/((self.X**2+self.Y**2)**1.5)
-        self.Vx = 0
-        self.Vy = Vy
+        self.MH = MH
+        self.XP = XP
+        self.MP = MP
+        self.XH = XH
+        self.YP = 0
+        self.YH = 0
+        self.VxP = 0
+        self.VxH = 0
+        self.VyP = VyP
+        self.VyH = VyH
 
     def calc_pos(self):
         global G
-        self.X = self.X+(self.Vx*self.T)
-        self.Y = self.Y+(self.Vy*self.T)
-        self.ax = -1*G*self.M*self.X/((self.X**2+self.Y**2)**1.5)
-        self.ay = -1*G*self.M*self.Y/((self.X**2+self.Y**2)**1.5)
-        self.Vx = self.Vx+self.ax*self.T
-        self.Vy = self.Vy+self.ay*self.T
-        return (self.X, self.Y)
+        self.DX = self.XP-self.XH
+        self.DY = self.YP-self.YH
+        #pythag theorem to find straight distance from a to b
+        linear_distance = (self.DX**2+self.DY**2)**0.5
+        #multiplies gravitational force by distance (X or Y) divided by total distance
+        self.X_force = (G*((self.MH*self.MP)/linear_distance**2))*(self.DX/linear_distance)
+        self.Y_force = (G*((self.MH*self.MP)/linear_distance**2))*(self.DY/linear_distance)
+        #updates velocities using a=f/m with force components as f
+        self.VxP -= self.X_force/self.MP*self.T
+        self.VxH += self.X_force/self.MH*self.T
+        self.VyP -= self.Y_force/self.MP*self.T
+        self.VyH += self.Y_force/self.MH*self.T
+        #changes position according to updated velocities
+        self.XP += self.VxP*self.T
+        self.XH += self.VxH*self.T
+        self.YP += self.VyP*self.T
+        self.YH += self.VyH*self.T
+        return (self.XP, self.YP, self.XH, self.YH)
