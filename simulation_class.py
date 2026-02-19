2905290a9634089b34609b06d2752a5d8d951845
jrandall24 <jrandall27@gcds.net>
2026-02-19 13:20:37 -0500

    updation

--- simulation_class.py
+++ simulation_class.py
@@ -2,46 +2,23 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 class ball:
-    def __init__(self,color,x=0,y=0):
-        self.x = x
-        self.y = y
-        self.color = (0,0,0)
-    def goto(self,x,y):
-        self.x = x
-        self.y = y
-    def plot(self,radius = 8):
-        plt.plot(self.x, self.y, marker="o", markersize=radius, markeredgecolor=self.color, markerfacecolor=self.color)
+    def __init__(self,Xs,Ys):
+        self.Xs = Xs
+        self.Ys = Ys
 
 class simulation:
     # init function runs with class is created
     # self argument is always needed and automatically passed 
-    def __init__(self,trail_color = "green"):
+    def __init__(self,trail_color = "black"):
         # Defines the class attributes Xs and Ys
         # An attribute is a variable specific to a class
-        self.balls = []
         self.trail_color = trail_color
-        #self.trail_line, = plt.plot([], [], color=self.trail_color, alpha=0.5)
-    def clear_balls(self):
-        self.balls.clear()
-    def add_ball(self,x=0,y=0,col = None):
-        if col == None:
-            col = self.trail_color
-        self.balls.append(ball(col,x,y))
-    def draw(self,size=8,with_trails = False):
-        for ball in self.balls:
-            ball.plot(radius=size)
-        if with_trails:
-            Xs = [i.x for i in self.balls]
-            Ys = [i.y for i in self.balls]
-            plt.plot(Xs, Ys, color=self.trail_color)
-    def draw_last_trails(self):
-        plt.plot([i.x for i in self.balls][-2:], [i.y for i in self.balls][-2:], color=self.trail_color)
-        #self.trail.set_data(self.history_x, self.history_y)
-        #self.trail_line.set_data(self.history_x, self.history_y)
-        #self.history_x = self.history_x[-50:]
-        #self.history_y = self.history_y[-50:]
-
-        # REWRITE: Update the data instead of calling plt.plot()
-        #self.trail_line.set_data([i.x for i in self.balls], [i.x for i in self.balls])
-        #self.marker.set_data([self.x], [self.y])
-        #pass
+    def draw_planet(self, X1, Y1):
+        plt.cla()
+        plt.plot(X1, Y1, marker="o", color='black', markersize=8)
+    def draw_host(self, X2, Y2):
+        plt.plot(X2, Y2, marker="o", color='green', markersize=15)
+    def trail(self, Xs, Ys):
+        Xs = [i for i in Xs]
+        Ys = [i for i in Ys]
+        plt.plot(Xs, Ys, color=self.trail_color)
