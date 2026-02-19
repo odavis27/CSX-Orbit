315243660473c2f9b9337368f14a0f22e2dca9c9
jrandall24 <jrandall27@gcds.net>
2026-02-19 18:13:58 -0500

    updation

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from simulation_class import *
 new_sim = simulation()
 
 t = 0
-p = planet(M = 1.989e30, X = 1.5e8, Vy = 581000)
-p2 = planet(M = 5.97e24, X = 0.1, Vy = 0)
+p = planet(MH = 1.989e30, XP = 149999549.8, VyP = 5.81e5, MP = 5.97e24, XH = -450.224, VyH = 0)
 # p.M = 1.989e30
 # p.X = 1.5e8
 # p.Vy = 581000
@@ -21,16 +20,15 @@ host_Y = []
 def anim(i):
     global t, p
     pos = p.calc_pos()
-    planet_X.append(pos[0])
-    planet_Y.append(pos[1])
-    new_sim.draw_planet(planet_X[-1], planet_Y[-1])
-    new_sim.trail(planet_X, planet_Y)
-    pos2 = p2.calc_pos()
-    host_X.append(pos2[0])
-    host_Y.append(pos2[1])
-    new_sim.draw_host(host_X[-1], host_Y[-1])
+    # planet_X.append(pos[0])
+    # planet_Y.append(pos[1])
+    # new_sim.draw(planet_X[-1], planet_Y[-1])
+    # new_sim.trail(planet_X, planet_Y)
+    host_X.append(pos[2])
+    host_Y.append(pos[3])
+    new_sim.draw(host_X[-1], host_Y[-1])
     new_sim.trail(host_X, host_Y)
-    print(p.X)
+    print(p.XH)
     t += p.T
 
 fig, ax = plt.subplots()
